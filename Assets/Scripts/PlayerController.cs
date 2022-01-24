3ed9826c0678e440e72320c44fd6a01a868beb62
William <awshalifax@gmail.com>
2022-01-23 20:01:52 -0400

    Adding first animations and script to move player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     public Rigidbody2D theRB;
     public float moveSpeed;
+    public Animator myAnim;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,19 @@ public class PlayerController : MonoBehaviour
     {
         
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+        //Variables are set in the Animator in Player
+        myAnim.SetFloat("moveX", theRB.velocity.x);
+        myAnim.SetFloat("moveY", theRB.velocity.y);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || 
+           Input.GetAxisRaw("Horizontal") == -1 || 
+           Input.GetAxisRaw("Vertical") == 1 || 
+           Input.GetAxisRaw("Vertical") == -1) {
+
+            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
 
     }
 }
